d4e80bd6becf34244728b2c76ea909152f0e0953
Ania Bieniek <anek12335@wp.pl>
2023-09-26 21:09:01 +0200

    summer.py

---
+++ summer.py
@@ -0,0 +1,11 @@
+shopping_dict = {}
+shopping_dict["piekarnia"] = ["bułka", "pączek"]
+shopping_dict["warzywniak"] = ["jabłbko", "marchew"]
+
+raport = ""
+for shop in shopping_dict:
+    raport += f"Idę do {shop.capitalize()} kupuję tam: {str(shopping_dict[shop]).title()}\n"
+print(raport)
+
+
+
