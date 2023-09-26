774fc12d5632f9b9746b96b2db0f9312e61dd988
Ania Bieniek <anek12335@wp.pl>
2023-09-26 22:01:47 +0200

    spring.py

---
+++ spring.py
@@ -0,0 +1,14 @@
+
+for i in range(1, 101):
+    if i % 5 == 0:
+        print(i)
+        potęga = i**3
+        def potęguj(i, potęga):
+            if potęga == 0:
+                return i
+        print(potęga)
+print("Odp. Liczby od 1 do 100 podzielne przez 5 w drugim wierszu i do potęgi 3.") 
+
+
+
+
