cac430933c0e394dc64cde4bab6bad5c600716e5
Ania Bieniek <anek12335@wp.pl>
2023-10-02 11:53:46 +0200

    interaction.py

--- interaction.py
+++ interaction.py
@@ -1,1 +1,32 @@
-interaction
+def shopping(items, payment='card', shop='lokal'):
+    result = ""
+    result = result + "Idę na zakupy do %s.\n" % shop
+    result = result + "Kupuję następujące rzeczy:\n"
+    for item in items:
+        result = result + " - %s\n" % item
+    result = result + "By zapłacić, użwyam %s." % payment
+    return result
+items = ["cola", "whiskey", "lód"]
+text = shopping(items, 'card', 'small local shop')
+print(text)
+
+print("Pokażę co wpiszesz!")
+tekst = input("dużo czekolady")
+
+print("Proszę kup:  ***%s***" % tekst)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
